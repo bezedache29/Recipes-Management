d683e111e069597e788e33ab51399a3c89c0788c
Tophe <bezedache29@gmail.com>
2022-02-21 16:16:47 +0100

    Installation de react-native + nettoyage + dark-mode

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1
+  }
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={isDarkMode ? Colors.dark : Colors.white}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default App;
